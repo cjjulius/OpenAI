6872d87f3280d115e40e107d340a2a29283c7ab4
Charlton E Julius <cjjulius@users.noreply.github.com>
2023-11-30 10:56:30 -0800

    Update test-openai-model.py

--- Labfiles/02-nlp-azure-openai/Python/test-openai-model.py
+++ Labfiles/02-nlp-azure-openai/Python/test-openai-model.py
@@ -1,8 +1,28 @@
 import os
 from dotenv import load_dotenv
 
-# Add Azure OpenAI package
+ # Add OpenAI import
+ from openai import AzureOpenAI
 
+ # Initialize the Azure OpenAI client
+ client = AzureOpenAI(
+         azure_endpoint = azure_oai_endpoint, 
+         api_key=azure_oai_key,  
+         api_version="2023-05-15"
+         )
+    
+ # Send request to Azure OpenAI model
+ response = client.chat.completions.create(
+     model=azure_oai_model,
+     temperature=0.7,
+     max_tokens=120,
+     messages=[
+         {"role": "system", "content": "You are a helpful assistant."},
+         {"role": "user", "content": "Summarize the following text in 20 words or less:\n" + text}
+     ]
+ )
+    
+ print("Summary: " + response.choices[0].message.content + "\n")
 
 def main(): 
         
